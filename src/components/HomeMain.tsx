9d86c0606dfa0b759126d93605ea11b7474adfd5
where.is.one <ohdearteddy@gmail.com>
2026-05-13 15:28:12 +0300

    Remove unused imports and add TypeScript error suppression comment in ScriptLoader

--- src/components/HomeMain.tsx
+++ src/components/HomeMain.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import { AppWindowMac, MapPin, Maximize } from "@deemlol/next-icons";
 
 export default function HomeMain() {
   return (
--- src/components/ScriptLoader.tsx
+++ src/components/ScriptLoader.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 
 export default function ScriptLoader() {
   useEffect(() => {
+    // @ts-expect-error: Suppressing TypeScript error as app.js is a third-party JS file without types.
     import('../styles/app.js');
   }, []);
 
